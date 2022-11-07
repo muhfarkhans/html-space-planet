1ef70c9c4c4dc36719af38b4812daa71d502223f
Muh Farkhan Syafii <zheninjas75@yahoo.co.id>
2022-11-07 10:30:29 +0700

    rename variable scroll to top button

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 // Get the button
-let buttonToTop = document.getElementById("buttonToTop");
+let scrollToTopButton = document.getElementById("scroll-to-top-button");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
-  scrollFunction();
+  showScrollToTopButton();
 };
 
-function scrollFunction() {
-  //   console.log(" ");
-  //   console.log("total height: " + document.documentElement.scrollHeight);
-  //   console.log("scroll top 2: " + document.documentElement.scrollTop);
-  //   console.log(" ");
-  let bot = document.documentElement.scrollHeight - 1200;
-  if (document.documentElement.scrollTop > bot) {
-    buttonToTop.style.display = "block";
+function showScrollToTopButton() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopButton.style.display = "block";
   } else {
-    buttonToTop.style.display = "none";
+    scrollToTopButton.style.display = "none";
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+function scrollToTop() {
   window.scroll({ top: 0, behavior: "smooth" });
 }
